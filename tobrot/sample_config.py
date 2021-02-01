af6a8dbd1723e89d8f12ba228582fed4ff6279da
Amal809-svg <78370108+Amal809-svg@users.noreply.github.com>
2021-02-01 22:29:42 +0530

    Update sample_config.py

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -10,14 +10,14 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1616724553:AAEYi79y_wzX6uxvJE9hbPB0l8n6GUuMD2w")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "" )
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 1223216))
+    APP_ID = int(os.environ.get("APP_ID",) )
     API_HASH = os.environ.get("API_HASH")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 17d294b8cf43485bfd90d20b7d5b7ff6))
+    OWNER_ID = int(os.environ.get("OWNER_ID",) )
     # Get these values from my.telegram.org
     # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "t.me/lilpublicleech").split())
+    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
